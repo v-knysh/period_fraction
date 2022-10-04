826adc81b3199e51cbb9faefc16ddb895f2d97b0
v-knysh <knyshv2412@gmail.com>
2022-10-04 21:24:30 +0300

    made my code super complicated, also added tests

--- fractions.py
+++ fractions.py
@@ -1,28 +1,64 @@
+from dataclasses import dataclass
 import datetime
-
-start = datetime.datetime(2022,3,13)
-
-
-def fraction(value, latest=60):
-   res = (0, 0, 0)
-   for b in range(latest, 0, -1):
-       a = int(value * b + 0.5)
-       precision = a/b - value
-       pr_lte_day = precision >= 0 and precision < 1/365
-       if pr_lte_day:
-           res = [a, b, precision]
-   return res
+import unittest
 
 
-print(fraction(0.5))
+@dataclass
+class CommonFraction:
+    base:float
+    numenator: int
+    denumenator: int
+    approx_error:float
+
+    @classmethod
+    def from_float(cls, 
+                   base:float, 
+                   max_denumenator:int=60,
+                   min_approx_error=0, 
+                   max_approx_error=1/365) -> "CommonFraction":
+        res = (1, 1, 1)
+        for b in range(max_denumenator, 0, -1):
+            a = int(base * b + 0.5)
+            approx_error = a/b - base
+            approx_matches = (approx_error >= min_approx_error 
+                and approx_error < max_approx_error)
+            if approx_matches:
+                res = (a, b, approx_error)
+        return cls(base, res[0], res[1], res[2])
+
+    def __str__(self):
+        return f"{self.numenator} / {self.denumenator}, {self.approx_error * 365}"
+
+    def value(self):
+        return self.numenator / self.denumenator
 
 
-for i in range(1, 366):
-    datestring = ((start + datetime.timedelta(days=i)).strftime("%y-%m-%d"))
+class TestUtils(unittest.TestCase):
+    def assert_equals(self, *pairs):
+        a, b = zip(*pairs)
+        self.assertEqual(a, b)
 
-    f = fraction(i/365, 365)
-    if f[1] > 20:
-        print(datestring)
-    else:
-        print(f"{datestring}, {f[0]} / {f[1]}, {f[2]*100 : .3f}")
+    def test_half_is_half(self):
+        f = CommonFraction.from_float(0.5)
+        self.assert_equals(
+            (f.numenator, 1), 
+            (f.denumenator, 2), 
+            (f.approx_error, 0)
+            )
+    
+    def test_182_days_is_half(self):
+        f = CommonFraction.from_float(182/365)
+        self.assert_equals(
+            (f.numenator, 1), 
+            (f.denumenator, 2), 
+            )
+    
+    def test_121_days_is_third(self):
+        f = CommonFraction.from_float(121/365)
+        self.assert_equals(
+            (f.numenator, 1), 
+            (f.denumenator, 3), 
+            )
 
+if __name__ == "__main__":
+    unittest.main()
