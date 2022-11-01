74de501f1def30de8cc9a5007c1dbdd7a9998438
v-knysh <knyshv2412@gmail.com>
2022-11-01 21:16:34 +0200

    fixed settings

--- settings.py
+++ settings.py
@@ -7,11 +7,6 @@ TG_BOT_API_TOKEN = os.environ.get("TG_BOT_API_TOKEN", "TG_BOT_API_TOKEN")
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "HEROKU_APP_NAME")
 
 
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TG_BOT_API_TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.getenv('PORT', 8888))
@@ -25,3 +20,10 @@ try:
         locals().update(data)
 except FileNotFoundError:
     pass
+
+
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TG_BOT_API_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
