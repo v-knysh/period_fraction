c10aec752eaaee9220600a4f2a6c760f25b56e9b
Vladyslav Knysh <knyshv2412@gmail.com>
2023-05-25 21:00:23 +0300

    fixed handler func

--- bot/report.py
+++ bot/report.py
@@ -19,7 +19,7 @@ async def send_report(message: types.Message, regexp_command):
 @dp.message_handler(
     filters.RegexpCommandsFilter(regexp_commands=['report_until_(?P<year>\d{4})_(?P<month>\d{2})_(?P<day>\d{2})'])
 )
-async def send_report(message: types.Message, regexp_command):
+async def send_report_until(message: types.Message, regexp_command):
     start_date = datetime.datetime(
         int(regexp_command.group('year')),
         int(regexp_command.group('month')),
